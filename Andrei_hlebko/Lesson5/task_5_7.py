b5ed35398c67c379395a644604ccaa679f8451e6
andrgit <andrgit1@gmail.com>
2021-10-11 17:00:34 +0200

    Lesson5, Homework 5_7

---
+++ Andrei_hlebko/Lesson5/task_5_7.py
@@ -0,0 +1,18 @@
+spis1 = []
+spis2 = [1,2,3]
+spis3 = [1.1,2.2,3.3]
+spis4 = [4,5,6]
+spis5 = [-5,-7,-3,-12]
+
+#
+x = 0
+for i in spis2:
+    x += i
+print(x)
+
+
+#Цикл For с range
+x = 0
+for i in range(101):
+    x += i
+print(x)
