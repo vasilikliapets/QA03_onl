a945f2bdf5cbee61091e7327267d9ea2d8797c32
andrgit <andrgit1@gmail.com>
2021-10-15 20:33:15 +0200

    Lesson6, Homework 6_3

---
+++ Andrei_hlebko/Lesson6/task_6_3(kalkulator_v0.1).py
@@ -0,0 +1,26 @@
+def calculator(chislo1, chislo2, oper):
+    """
+    This function work as a simple calculator
+    choose operators, input 2 values, get result
+    """
+    chislo1 = int(chislo1)
+    chislo2 = int(chislo2)
+    if oper == "1":
+        return chislo1 + chislo2
+    elif oper == "2":
+        return chislo1 - chislo2
+    elif oper == "3":
+        return chislo1 * chislo2
+    elif oper == "4":  # +проверка деления на 0
+        if chislo1 == 0 or chislo2 == 0:
+            return "Деление на 0!"
+        else:
+            return f"Частное (целочисленное деление): {chislo1 // chislo2}, Остаток: {chislo1 % chislo2}"
+    else:
+        print("Выберите из предложенного списка")
+
+
+oper_chisla = input("Выберите необходимую операцию: 1 = Сложение, 2 = Вычитание, 3 = Умножение, 4 = Деление: ")
+chislo1 = input("Введите первое число: ")
+chislo2 = input("Введите второе число: ")
+print(calculator(chislo1, chislo2, oper_chisla))
