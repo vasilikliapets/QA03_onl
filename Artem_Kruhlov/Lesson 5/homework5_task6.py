1d8e7ca2862f1c1eb54b0270457e3e4bdb98a341
Artem-Kruhlov <63775314+Artem-Kruhlov@users.noreply.github.com>
2021-10-16 21:00:04 +0300

    Update homework5_task6.py

--- Artem_Kruhlov/Lesson 5/homework5_task6.py
+++ Artem_Kruhlov/Lesson 5/homework5_task6.py
@@ -1,8 +1,8 @@
 list_1 = list(input())
 list_2 = []
 for i in range(len(list_1)):
-    current_char = ord(list_1[i]) # находит символ по юникоду
-    if 65 <= current_char <= 90: # в этих пределах находятся все заглавные буквы
+     # находит символ по юникоду
+    if list_1[i].isupper(): # в этих пределах находятся все заглавные буквы
         list_2.append(list_1[i]) # если список в пределах от 65 до 90, добавляет это значение в list_2
 result = len(list_2) == 0 # если длинна списка равно 0 выводит True
 print([result, list_2])
