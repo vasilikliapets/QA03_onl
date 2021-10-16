fd0a22a2a384d6f541e0554b417782a60aaccd65
PavelBohdan <lllhypok87@gmail.com>
2021-10-16 16:22:59 +0300

    Homework4:Pavel_Bohdan

--- Pavel_Bohdan/Lesson_4/tms_4.py
+++ Pavel_Bohdan/Lesson_4/tms_4.py
@@ -78,5 +78,3 @@ print('Summ is {sum} and diff = {dif}'.format(sum = a + b, dif = a - b))
 #2)
 list_of_children = ['Sasha', 'Vasia', 'Nikalai']
 print(f'First child is {list_of_children[0]}, second is {list_of_children[1]}, and last one - {list_of_children[2]}')
-
-
