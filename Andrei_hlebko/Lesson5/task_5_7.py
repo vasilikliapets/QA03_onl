15e7f0547a32e95d0c89e9ff3bee8fe28a1c862f
andrgit <andrgit1@gmail.com>
2021-10-17 20:37:51 +0200

    task 5_7 bug fixed

--- Andrei_hlebko/Lesson5/task_5_7.py
+++ Andrei_hlebko/Lesson5/task_5_7.py
@@ -1,8 +1,8 @@
 spis1 = []
-spis2 = [1,2,3]
-spis3 = [1.1,2.2,3.3]
-spis4 = [4,5,6]
-spis5 = [-5,-7,-3,-12]
+spis2 = [1, 2, 3]
+spis3 = [1.1, 2.2, 3.3]
+spis4 = [4, 5, 6]
+spis5 = [-5, -7, -3, -12]
 
 #
 x = 0
@@ -10,11 +10,11 @@ for i in spis2:
     x += i
 print(x)
 
+# другой вариант через sum
 print(sum(spis2))
 print(sum(range(101)))
 
-
-#Цикл For с range
+# Цикл For с range
 x = 0
 for i in range(101):
     x += i
