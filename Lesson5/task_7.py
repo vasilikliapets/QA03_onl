d880e9792fd2964f2cca32c3aa3841f8f87c1556
Hanna Dziahel <dyagelaa@gmail.com>
2021-10-18 22:50:34 +0300

    Change tasks 1, 5, 7 in hw5

--- Lesson5/task_7.py
+++ Lesson5/task_7.py
@@ -5,7 +5,7 @@ list_1 = []
 list_2 = [1, 2, 3]
 list_3 = [1.1, 2.2, 3.3]
 list_4 = [4, 5, 6]
-list_5 = [i for i in range(101)]
+list_5 = range(101)
 # Суммирование всех чисел в списке через функцию sum()
 print(sum(list_1))
 print(sum(list_2))
