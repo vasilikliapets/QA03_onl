64b1532eb876e53d30be26411e6e9afaea05db7f
Hanna Dziahel <dyagelaa@gmail.com>
2021-10-21 22:48:49 +0300

    Add deleted folder Lesson1

--- Hanna_Dziahel/Lesson1/task_1_5.py
+++ Hanna_Dziahel/Lesson1/task_1_5.py
@@ -6,5 +6,5 @@ hypotenuse_of_triangle = (a ** 2 + b ** 2) ** 0.5
 # Найти площадь прямоугольного треугольника
 square_of_triangle = 0.5 * (a * b)
 
-print(hypotenuse_of_triangle)
+print("hypotenuse_of_triangle = {}".format(hypotenuse_of_triangle))
 print(square_of_triangle)
