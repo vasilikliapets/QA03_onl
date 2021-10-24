ec4f064a48e9adddaaba5337df5533773823b84c
Anastasiya Odintsova <92889671+AnastasiyaOdintsova@users.noreply.github.com>
2021-10-24 20:07:49 +0200

    Create Task_4.py

---
+++ Anastasiya_Odintsova/Lesson 4/Task_4.py
@@ -0,0 +1,57 @@
+#Списки
+#1
+l=[1,2,3]
+l1=[4,5,6]
+
+#2
+print(l[1])
+
+#3
+l1[2]=7
+print(l1)
+
+#4
+l2=l+l1
+print(l2)
+
+#5
+l3=l2[1:4]
+print(l3)
+
+#6
+l2.insert(2,10)
+l2.insert(3,20)
+print(l2)
+
+#7
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(list((set(a)).intersection(set(b))))
+
+#8
+c=[1, 2, 3, 4, 3, 2, 5, 1, 4, 6, 7, 1, 8, 2, 3]
+print(list(set(c)))
+
+#Логические операции
+#1
+a=1
+b=5
+
+#2
+print(a==1 and b==5)
+print(a>0 and b>a)
+print(a==b and b==a)
+print(a!=a and b!=b)
+
+#3
+print(a==a or b==b)
+print(a>0 or b>0)
+print(a==b or b==a)
+print(a>b or b<a)
+
+#4
+c='Python is a language'
+print(
+
+#Словари
+#1
