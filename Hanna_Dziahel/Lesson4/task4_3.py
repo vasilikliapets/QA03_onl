7503a2553c14302e71225dbe9664c4bed396a5b0
Anastasiya Odintsova <92889671+AnastasiyaOdintsova@users.noreply.github.com>
2021-10-30 17:27:51 +0200

    Delete Hanna_Dziahel directory

--- Hanna_Dziahel/Lesson4/task4_3.py
+++
@@ -1,7 +0,0 @@
-# 3 Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
-# сделайте из него строку => "I love arrays they are my favorite"
-list = ["I", "love", "arrays", "they", "are", "my", "favorite"]
-# Использование метода join() для сбора строк списка в одну строку с разделителем,
-# который указан перед join
-string = " ".join(list)
-print(string)
