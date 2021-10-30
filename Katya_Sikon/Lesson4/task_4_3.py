511f9dfbb6e7de02dd845969465601a7447a5da6
Anastasiya Odintsova <92889671+AnastasiyaOdintsova@users.noreply.github.com>
2021-10-30 17:27:10 +0200

    Delete Katya_Sikon directory

--- Katya_Sikon/Lesson4/task_4_3.py
+++
@@ -1,7 +0,0 @@
-# 3. Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него строку => "I love arrays they are my favorite"
-
-l3 = ["I", "love", "arrays", "they", "are", "my", "favorite"]
-s3 = ' '.join(l3)
-
-print('\nсписок\n', l3)
-print('\nстрока из списка\n', s3)
