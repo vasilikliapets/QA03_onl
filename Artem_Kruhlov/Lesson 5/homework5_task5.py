1a41c5435ff654b53b37402691db54152afba99f
Anastasiya Odintsova <92889671+AnastasiyaOdintsova@users.noreply.github.com>
2021-10-30 17:28:28 +0200

    Delete Artem_Kruhlov directory

--- Artem_Kruhlov/Lesson 5/homework5_task5.py
+++
@@ -1,8 +0,0 @@
-list_1 = eval(input()) # вводим лист значений
-result = True
-for i in range(len(list_1)):
-    if list_1[i] != list_1[0]: # проверяем что значение в листе не равно значние в лист_1
-        result = False
-        break
-
-print(result)
