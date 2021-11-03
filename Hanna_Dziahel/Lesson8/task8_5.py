f90450da4d609a72fd7cc5e9f335e946c73a8472
Hanna Dziahel <dyagelaa@gmail.com>
2021-11-03 23:19:33 +0300

    Add changes in task 8.5

--- Hanna_Dziahel/Lesson8/task8_5.py
+++ Hanna_Dziahel/Lesson8/task8_5.py
@@ -2,6 +2,11 @@
 # функция принимает все свои не именованные аргументы в порядке,
 # обратном тому, в котором их передали
 
+#функция принимает все свои не именованные аргументы в порядке,
+#обратном тому, в котором их передали
+#тут тебе надо вызывать функцию свою изначальную с аргументами в обратном
+# порядке, счас ты ее не вызываешь, а просто возвращаешь аргументы
+
 def decorator_change(func):
     """
     This function-decorator reverses values from decorated function
@@ -10,14 +15,14 @@ def decorator_change(func):
     """
     def wrapper(*tuples):
         new_tup = tuples[::-1]
-        return new_tup
+        return func(new_tup)
     return wrapper
 
 
 @decorator_change
 def show_positional_args(*args):
-    tuple_args = args
-    return tuple_args
+    list_args = list(*args)
+    return list_args
 
 
 print(show_positional_args(1, 2, 3, 4, 5, 6, 7))
