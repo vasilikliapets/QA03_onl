1683bef9115460c271be8f08f7b7b96b55fa427c
PavelBohdan <lllhypok87@gmail.com>
2021-11-04 08:55:20 +0300

    Homework8_extra

---
+++ Pavel_Bohdan/Lesson_8_extra/decor_8_extra.py
@@ -0,0 +1,21 @@
+import time
+import functools
+
+#1
+def caching(timeout):
+    """Декоратор кэширования результата"""
+    def outer_wrapper(func):
+        time_ref, value = 0, 0
+        def wrapper(*args):
+            nonlocal time_ref
+            nonlocal value
+            now = time.time()
+            if now - time_ref < timeout:
+                print(value)
+            else:
+                value = func(*args)
+                print(value)
+                time_ref = now
+            
+        return wrapper
+    return outer_wrapper
