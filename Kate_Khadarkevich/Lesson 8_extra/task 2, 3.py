4972b8241e8c4d56bfdd770e96685f462dd3c500
KateKhadarkevich <91566036+KateKhadarkevich@users.noreply.github.com>
2021-11-20 14:04:08 +0300

    Update task 2, 3.py

--- Kate_Khadarkevich/Lesson 8_extra/task 2, 3.py
+++ Kate_Khadarkevich/Lesson 8_extra/task 2, 3.py
@@ -1,6 +1,6 @@
 # 2. Реализуйте лямбда функцию, которая будет сортировать список по убыванию и возвращать его
 one_list = [4, 11, 5, 3, 7, 14]
-sorteist = lambda x: sorted(x)[::-1]
+sorted_list = lambda x: sorted(x)[::-1]
 print(sorted_list(one_list))
 
 
