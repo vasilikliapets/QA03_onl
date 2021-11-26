16094a10102bb74890903b8585334fe040758705
Ilya Markevich <91626196+MarkevichIlya@users.noreply.github.com>
2021-11-26 16:13:11 +0300

    Update task_8.py

--- Ilya_Markevich/Lesson_8/task_8.py
+++ Ilya_Markevich/Lesson_8/task_8.py
@@ -1,20 +1,19 @@
 from task_8_dec import dec_1, dec_2, dec_3, dec_4, dec_5, dec_6
 
-@timer
-@func_name
+@dec_4
+@dec_6
 def fibonacci(n):
     a, b = 1, 1
     for i in range(n):
-        yield a
         a, b = b, a + b
     return a, b
 
-data = fibonacci(10)
+data = fibonacci(3)
 print(data)
 
 
 @dec_1
 @dec_2
 def sl(a, b, c):
-    answer = ((a * a ** c) / b) * 0,5
+    answer = ((a * a ** c) / b) * 0.5
     return answer
