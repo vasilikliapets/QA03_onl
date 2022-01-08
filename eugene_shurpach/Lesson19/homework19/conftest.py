d0d3d13efcc45a009b66246542d01c26692507c9
eshurpach <eshur@eshur.ru>
2022-01-08 20:19:28 +0300

    Added homework19

---
+++ eugene_shurpach/Lesson19/homework19/conftest.py
@@ -0,0 +1,22 @@
+import time
+import pytest
+
+start = time.perf_counter()
+
+
+@pytest.fixture(scope='module')
+def get_time():
+    print(f"\nВремя старта: {start}")
+    yield
+    print(f"\nВремя завершения: {time.perf_counter()}")
+    return start
+
+
+@pytest.fixture(scope='module')
+def get_name(request):  # фикстура вывода имени модуля
+    print(f'Тесты из файла: {request.node.name}')
+
+@pytest.fixture
+def count_time():
+    end = time.perf_counter()
+    print(f"\nВремя выполения от начала: {end - start}")
