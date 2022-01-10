c9c680cfc3b654c6ac036aeb6f2918a9003b0758
Anastasiya Odintsova <92889671+AnastasiyaOdintsova@users.noreply.github.com>
2022-01-10 11:16:28 +0300

    Update HomeWork_7_2.py

--- Anastasiya_Odintsova/Lesson 7/HomeWork_7_2.py
+++ Anastasiya_Odintsova/Lesson 7/HomeWork_7_2.py
@@ -1,9 +1,12 @@
 
 # 2
 def encode(letter, shift):
-    """
-    Функция кодирует фразу
-    """
+  """
+  Функция кодирует и декодирует фразу
+  :param letter: фраза
+  :param shift: значение шифрования
+  :return: кодированая и декодированная фразы
+  """
   first_code = None
   last_code = None
 
@@ -18,49 +21,16 @@ def encode(letter, shift):
 
   letters_count = last_code - first_code + 1
   code = (ord(letter) - first_code + shift) % letters_count + first_code
-  result = chr(code)
-  return result
+  res1 = chr(code)
 
-def decode(letter, shift):
-    """
-    Функция декодирует фразу
-    """
-  first_code = None
-  last_code = None
+  letters_count_2 = last_code - first_code - 1
+  code_2 = (ord(res1) - first_code - shift) % letters_count_2 + first_code
+  res2 = chr(code_2)
+  return res1, res2
 
-  if ord('a') <= ord(letter) <= ord('z'):
-    first_code = ord('a')
-    last_code = ord('z')
-  elif ord('A') <= ord(letter) <= ord('Z'):
-    first_code = ord('A')
-    last_code = ord('Z')
-  else:
-    return letter
 
-  letters_count = last_code - first_code - 1
-  code = (ord(letter) - first_code - shift) % letters_count + first_code
-  result = chr(code)
-  return result
-
-def choice_user():
-    """
-    Функция выбора действия кодировать/декодировать
-    """
-  while True:
-    print("Выберите операцию:\n1.Кодировать:\n2.Декодировть:\n")
-    choice = input("Введите номер пункта меню: \n")
-    if choice not in ("1", "2"):
-        print("Мимо, попробуй еще раз!")
-        choice = input(f"Выберите операцию:\n1.Кодировать:\n2.Декодировть:\n")
-    if choice == "1":
-        message = input('Введите фразу: ')
-        shift = int(input('Значение шифрования: '))
-        for letter in message:
-          print(encode(letter, shift))
-    if choice == "2":
-        message = input('Введите фразу: ')
-        shift = int(input('Значение шифрования: '))
-        for letter in message:
-          print(decode(letter, shift))
-
-choice_user()
+message = input('Введите фразу: ')
+shift = int(input('Значение шифрования: '))
+for letter in message:
+  res1, res2 = encode(letter, shift)
+  print(res1, res2)
