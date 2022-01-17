5ba600cf01acb23771a591b3767680cde41c7139
Ilya Markevich <91626196+MarkevichIlya@users.noreply.github.com>
2022-01-17 21:30:45 +0300

    Create Homework_18.1.py

---
+++ Ilya_Markevich/Lesson_18/Homework_18.1.py
@@ -0,0 +1,20 @@
+# Импортируем модуль platform для дальнейшего получения информации об
+# операционной системе
+import platform
+
+class OperationSystem:
+    @staticmethod
+    def get_name_of_os():
+        return platform.system()
+
+    @staticmethod
+    def get_release_of_os():
+        return platform.release()
+
+    @staticmethod
+    def get_version_of_os():
+        return platform.version()
+
+print(OperationSystem.get_name_of_os(), OperationSystem.get_release_of_os(), OperationSystem.get_version_of_os())
+
+
