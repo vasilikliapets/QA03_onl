f648b3ab105bf293554334b71571113b5d21090c
KateKhadarkevich <91566036+KateKhadarkevich@users.noreply.github.com>
2022-01-25 17:43:27 +0300

    Update test_OS.py
    поправила

--- Kate_Khadarkevich/Lesson18/test_OS.py
+++ Kate_Khadarkevich/Lesson18/test_OS.py
@@ -15,10 +15,10 @@ class TestPositive(unittest.TestCase):
 
 class TestNegative(unittest.TestCase):
     def test_name(self):
-        self.assertEqual(operSystem.OperationSystem.name_os(), "Linux Ubuntu")
+        self.assertNotEqual(operSystem.OperationSystem.name_os(), "Linux Ubuntu")
 
     def test_release(self):
-        self.assertEqual(operSystem.OperationSystem.release_os(), "9")
+        self.assertNotEqual(operSystem.OperationSystem.release_os(), "9")
 
     def test_version(self):
-        self.assertEqual(operSystem.OperationSystem.version_os(), "20.04")
+        self.assertNotEqual(operSystem.OperationSystem.version_os(), "20.04")
