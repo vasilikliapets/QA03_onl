dafa771d1b0068ee03975d151ddd3c1495b2fa1e
Shcherbakov-Sergey-Alch <91654713+Shcherbakov-Sergey-Alch@users.noreply.github.com>
2022-02-01 13:46:53 +0300

    Update Test_OS18.py

--- Sergey_Shcherbakov/Lesson18/Test_OS18.py
+++ Sergey_Shcherbakov/Lesson18/Test_OS18.py
@@ -15,10 +15,10 @@ class TestPositive(unittest.TestCase):
 
 class TestNegative(unittest.TestCase):
     def test_name(self):
-        self.assertEqual(Info_OS18.OS.os_name(), "Mac OS")
+        self.assertNotEqual(Info_OS18.OS.os_name(), "Mac OS")
 
     def test_release(self):
-        self.assertEqual(Info_OS18.OS.os_release(), "12")
+        self.assertNotEqual(Info_OS18.OS.os_release(), "12")
 
     def test_version(self):
-        self.assertEqual(Info_OS18.OS.os_version(), "12.2")
+        self.assertNotEqual(Info_OS18.OS.os_version(), "12.2")
