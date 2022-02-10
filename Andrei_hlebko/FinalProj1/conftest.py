b108a141a157593a7dfe33d6da4675cd4fcbdd89
andrgit <86713245+andrgit@users.noreply.github.com>
2022-02-10 22:40:19 +0100

    Update conftest.py

--- Andrei_hlebko/FinalProj1/conftest.py
+++ Andrei_hlebko/FinalProj1/conftest.py
@@ -10,18 +10,18 @@ from selenium import webdriver
 #     yield driver
 #     driver.quit()
 
+#Для запуска на jenkins Mac Andrei
 @pytest.fixture(scope='session')
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument("--remote-debugin-port=9222")
-    options.add_argument("--screen-size=1200x800")
-    driver = webdriver.Chrome(options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(5)
-    yield
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture(scope='session')
