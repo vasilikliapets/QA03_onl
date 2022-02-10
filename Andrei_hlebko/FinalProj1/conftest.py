427285f1adfa309386351b2248bd8f67f998700b
andrgit <andrgit1@gmail.com>
2022-02-10 13:55:21 +0100

    FinalProjTMS

--- Andrei_hlebko/FinalProj1/conftest.py
+++ Andrei_hlebko/FinalProj1/conftest.py
@@ -4,11 +4,24 @@ import pytest
 from selenium import webdriver
 
 
+# @pytest.fixture(scope='session')
+# def driver():
+#     driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
+
 @pytest.fixture(scope='session')
 def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--remote-debugin-port=9222")
+    options.add_argument("--screen-size=1200x800")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    yield
 
 
 @pytest.fixture(scope='session')
